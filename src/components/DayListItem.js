f543b8c5969eb0600188b917fc2796927826393e
KelseyErickson <kerickson@gmx.com>
2021-11-09 14:37:36 -0700

    Add messages with conditions for DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,16 +3,30 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const {selected, spots, name} = props;
+
   const dayClass = classNames("day-list__item", 
   {
-    "day-list__item--selected": props.selected, 
-    "day-list__item--full": !(props.spots)
-
+    "day-list__item--selected": selected, 
+    "day-list__item--full": !spots
   });
+
+  const formatSpots = (spots) => {
+    if(!spots){
+      return "no spots remaining";
+    }
+    if(spots === 1){
+      return "1 spot remaining";
+    }
+    if(spots > 1){
+      return `${spots} spots remaining`;
+    }
+  };
+
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+    <li className={dayClass} onClick={() => props.setDay(name)}>
+      <h2 className="text--regular">{name}</h2> 
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
