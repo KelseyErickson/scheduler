e482e8742f3afc6231ea6edade54690a5e3a05f3
KelseyErickson <kerickson@gmx.com>
2021-11-13 10:01:59 -0700

    Fix the Appointment component to return an article instead of a Fragment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
@@ -6,9 +6,16 @@ import Empty from "./Empty";
 
 export default function Appointment(props) {
   return (
-    <Fragment>
+    <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
-    </Fragment>
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
   );
 }
