7cfe5d3bc843a5f3fe5889af367321f15ee414c9
KelseyErickson <kerickson@gmx.com>
2021-11-13 09:55:01 -0700

    Add conditional Show and Empty components and props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,18 +1,14 @@
-import React from "react";
-import "components/Appointment/styles.scss"
+import React, {Fragment} from "react";
+import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-
-
-  const availableAppointments = () => {
-    if(!props.time){
-      return "No Appointments";
-    } else {
-      return `Appointment at ${props.time}`
-    }
-  };
-
   return (
-    <article className="appointment">{availableAppointments()}</article>
+    <Fragment>
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </Fragment>
   );
 }
