edcbeb4ce4bdc2a491dd8402cfa1a12613fe7440
KelseyErickson <kerickson@gmx.com>
2021-11-15 16:24:48 -0700

    Remove dummy data and connect to api with useEffect and axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   1: {
@@ -63,14 +45,22 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/days")
+      .then((response) => {
+        console.log(response)
+        setDays(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.status);
+      });
+  }, []);
 
   const appointmentArray = Object.values(appointments).map((appointment) => {
-    return (
-    <Appointment
-      key={appointment.id}
-      {...appointment}
-    />
-    );
+    return <Appointment key={appointment.id} {...appointment} />;
   });
   return (
     <main className="layout">
