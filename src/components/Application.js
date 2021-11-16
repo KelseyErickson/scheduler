919c6f1dab627b22a48a9aade160b89ecac302de
KelseyErickson <kerickson@gmx.com>
2021-11-16 14:40:13 -0700

    Add state object, helper functions and adjust variables.

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,14 +44,20 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // appointments: {}
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
 
   useEffect(() => {
     axios
       .get("/api/days")
       .then((response) => {
-        console.log(response)
         setDays(response.data);
       })
       .catch((error) => {
@@ -62,6 +68,7 @@ export default function Application(props) {
   const appointmentArray = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -72,7 +79,10 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList 
+          days={state.days} 
+          value={state.day} 
+          onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
