86848afe6b5bc0de70a022c832f0ac333d97a418
KelseyErickson <kerickson@gmx.com>
2021-11-18 16:08:17 -0700

    Import getInterviewerForDay and use to pass interviewer array to Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -12,8 +12,6 @@ export default function Application(props) {
     appointments: {},
     interviewers: {}
   });
-
- 
 
   const setDay = day => setState({ ...state, day });
   
@@ -27,17 +25,18 @@ export default function Application(props) {
     });
   }, []);
 
+  const interviewersArray = getInterviewersForDay(state, state.day);
   const appointments = getAppointmentsForDay(state, state.day);
   
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-  
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={interviewersArray}
       />
     );
   });
