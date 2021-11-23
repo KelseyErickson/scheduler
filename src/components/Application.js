603d0fc7352fadb3ac4c639ba0b7dfdbdaee993b
KelseyErickson <kerickson@gmx.com>
2021-11-23 13:51:51 -0700

    Add axios put request to update appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,13 +31,20 @@ export default function Application(props) {
       ...state.appointments[id],
       interview: { ...interview }
     };
+   
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
 
-    setState({...state, appointments});
-    
+
+
+   return axios.put(`/api/appointments/${id}`, {interview})
+     .then(() => {
+       setState({...state, appointments})
+     })
+     .catch(err => console.log(err.response))
+  
     
   }
   
