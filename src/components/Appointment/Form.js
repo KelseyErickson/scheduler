1c524c850562ec517b11b30ae93d70baa09d85a4
KelseyErickson <kerickson@gmx.com>
2021-11-23 16:27:32 -0700

    Adjust form to take in interviewer id as props

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,47 +1,51 @@
-import React, {useState} from "react";
-import Button from "components/Button"
+import React, { useState } from "react";
+import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);
-
+  const [interviewer, setInterviewer] = useState(props.interviewer.id || null);
+  
   const reset = () => {
     setStudent("");
     setInterviewer(null);
-  }
+  };
 
-  const cancel = () =>{
+  const cancel = () => {
     reset();
     props.onCancel();
-  }
+  };
+
 
   return (
     <main className="appointment__card appointment__card--create">
-    <section className="appointment__card-left">
-      <form onSubmit={event => event.preventDefault()} autoComplete="off">
-        <input 
-          className="appointment__create-input text--semi-bold"
-          name="name"
-          type="text"
-          placeholder="Enter Student Name"
-          value = {student}
-          onChange={(event) => setStudent(event.target.value)}
+      <section className="appointment__card-left">
+        <form onSubmit={(event) => event.preventDefault()} autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+          value={interviewer}
         />
-      </form>
-      <InterviewerList 
-        interviewers={props.interviewers}
-        onChange={setInterviewer}
-        value={interviewer}
-      />
-    </section>
-    <section className="appointment__card-right">
-      <section className="appointment__actions">
-        <Button danger onClick={cancel}>Cancel</Button>
-        <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
       </section>
-    </section>
-  </main>
-  
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
   );
 }
