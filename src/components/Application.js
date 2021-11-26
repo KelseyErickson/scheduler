e932787e21208d765dd525cc85dd52bcd8c17545
KelseyErickson <kerickson@gmx.com>
2021-11-26 13:18:36 -0700

    Clean up code and add comments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,29 +2,27 @@ import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview,getInterviewersForDay } from "helpers/selectors";
 import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  const {
-    state,
-    setDay,
-    bookInterview,
-    cancelInterview
+  const { 
+    state, 
+    setDay, 
+    bookInterview, 
+    cancelInterview 
   } = useApplicationData();
 
-  const interviewersArray = getInterviewersForDay(state, state.day);
   const appointments = getAppointmentsForDay(state, state.day);
-  
+  // Get apppointments for day and return schedule to display current state of appointments
   const schedule = appointments.map((appointment) => {
-  const interview = getInterview(state, appointment.interview);
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
-        interview={interview}
-        interviewers={interviewersArray}
+        interview={getInterview(state, appointment.interview)}
+        interviewers={getInterviewersForDay(state, state.day)}
         bookInterview={bookInterview}
         cancelInterview={cancelInterview}
       />
@@ -41,10 +39,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList 
-          days={state.days} 
-          value={state.day} 
-          onChange={setDay} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
