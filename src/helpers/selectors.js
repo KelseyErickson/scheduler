43b1387b2c5ba2efa9db776e788d8328b19d3598
KelseyErickson <kerickson@gmx.com>
2021-11-26 13:35:39 -0700

    Code clean-up and comments

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,27 +1,29 @@
+// Function to take in state and day and return the appointments for that day as an array of objects
 export function getAppointmentsForDay(state, day) {
   let appointmentsForDay = [];
 
   if (!state.days.length) {
     return [];
   }
-
+  // Filter selected day from state.days array
   const filteredDays = state.days.filter((stateDay) => stateDay.name === day);
-
+  
   if (!filteredDays.length) {
     return [];
   }
-
+  // Place the appointments array for the filtered day in a variable
   const appointmentArray = filteredDays[0].appointments;
-
+  
   for (const appointment of appointmentArray) {
-    if (state.appointments[appointment]) {
-      appointmentsForDay.push(state.appointments[appointment]);
-    }
+  //Push state.appointments objects to the appointmentsForDay array
+    appointmentsForDay.push(state.appointments[appointment]);
   }
 
   return appointmentsForDay;
 };
 
+// Function to take in state and day return the interviewers for that day as an array of objects
+// Same basic funtionality as getAppointmentsForDay
 export function getInterviewersForDay(state, day) {
   let interviewersForDay = [];
 
@@ -38,19 +40,18 @@ export function getInterviewersForDay(state, day) {
   const interviewerArray = filteredDays[0].interviewers;
   
   for (const interviewer of interviewerArray) {
-    if (state.interviewers[interviewer]) {
-      interviewersForDay.push(state.interviewers[interviewer]);
-    }
+    interviewersForDay.push(state.interviewers[interviewer]);
   }
-
+  
   return interviewersForDay;
 };
 
+// Function to take in state and interview and return an object with interviewer and student
 export function getInterview(state, interview) {
   if(!interview){
     return null;
   }
-  const interviewerInfo = state.interviewers[interview.interviewer]
+  const interviewerInfo = state.interviewers[interview.interviewer];
 
   const interviewObject ={
     "student": interview.student,
@@ -59,8 +60,7 @@ export function getInterview(state, interview) {
       "name": interviewerInfo.name, 
       "avatar": interviewerInfo.avatar
     }
-  }
-
+  };
+  
   return interviewObject;
 };
-
