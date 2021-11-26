1581ee77fe7873c6936fab489b77561cb92862bf
KelseyErickson <kerickson@gmx.com>
2021-11-26 13:52:47 -0700

    Add comments and remove white space

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,24 +4,24 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+  // Function will take in a newMode and a boolean. Will set state depending on the boolean (replace)
   const transition = (newMode, replace = false) => {
     setMode(newMode);
-
+    // If replace is true overwrite previous item in  history array with newMode otherwise add the mode to the array
     if (replace) {
       const copyForReplace = [...history];
       copyForReplace[copyForReplace.length - 1] = newMode;
       setHistory(copyForReplace);
-      
     } else {
       setHistory((prev) => [...prev, mode]);
     }
   };
-
+  // Function to go back to previous mode
   const back = () => {
     if (history.length <= 1) {
       return;
     }
-
+    // Pop item off a copy of history, update history and update mode with last item in history array
     const copyHistory = [...history];
     copyHistory.pop();
     setHistory(copyHistory);
@@ -29,4 +29,4 @@ export default function useVisualMode(initial) {
   };
 
   return { mode, transition, back };
-}
+};
