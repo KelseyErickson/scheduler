2644b5cfb846661687a75f1613584cfaafdc1e8f
KelseyErickson <kerickson@gmx.com>
2021-12-02 14:37:42 -0700

    Add rest of test to test saving and adjusting spots remaining

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -5,11 +5,12 @@ import {
   cleanup,
   waitForElement,
   fireEvent,
-  getByText, 
-  prettyDOM, 
+  getByText,
+  prettyDOM,
   getAllByTestId,
-  getByAltText, 
-  getByPlaceholderText
+  getByAltText,
+  getByPlaceholderText,
+  queryByText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -28,24 +29,30 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
-
     const appointment = appointments[0];
 
     fireEvent.click(getByAltText(appointment, "Add"));
 
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-      target: { value: "Lydia Miller-Jones" }
+      target: { value: "Lydia Miller-Jones" },
     });
 
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-    
     fireEvent.click(getByText(appointment, "Save"));
 
-    console.log(prettyDOM(appointment));
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
