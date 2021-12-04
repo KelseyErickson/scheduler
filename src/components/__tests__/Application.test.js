bb43d8f5149d99b651bd91d2f499897c5c67f390
KelseyErickson <kerickson@gmx.com>
2021-12-04 09:27:39 -0700

    Add test for editing an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -11,6 +11,8 @@ import {
   getByAltText,
   getByPlaceholderText,
   queryByText,
+  getByDisplayValue,
+  queryByAltText
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -18,6 +20,7 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 describe("Application", () => {
+
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
 
@@ -56,39 +59,80 @@ describe("Application", () => {
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
+
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
-    const { container} = render(<Application />);
+    const { container, debug } = render(<Application />);
+
 
     // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
-
+     
     // 3. Click the "Delete" button on the booked appointment.
     const appointment = getAllByTestId(container, "appointment").find(
-      (appointment) => queryByText(appointment, "Archie Cohen")
+      appointment => queryByText(appointment, "Archie Cohen")
     );
-
-    fireEvent.click(getByAltText(appointment, "Delete"));
-
+  
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+  
     // 4. Check that the confirmation message is shown.
     expect(
       getByText(appointment, "Are you sure you would like to delete?")
     ).toBeInTheDocument();
-
+  
     // 5. Click the "Confirm" button on the confirmation.
     fireEvent.click(queryByText(appointment, "Confirm"));
-
+  
     // 6. Check that the element with the text "Deleting" is displayed.
     expect(getByText(appointment, "Deleting")).toBeInTheDocument();
-
+  
     // 7. Wait until the element with the "Add" button is displayed.
     await waitForElement(() => getByAltText(appointment, "Add"));
-
+  
     // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+
+  });
+  
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+0
+    // 3. Click the "Edit" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(getByAltText(appointment, "Edit"));
+    
+    //5. Change the information
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), { // What is this??
+      target: { value: "Jeff Bridges" },
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    // 6. Click Save Button.
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // 7. Wait until the element with updated informaion is displayed.
+
+    await waitForElement(() => getByText(appointment, "Jeff Bridges"));
+
+    expect(getByText(appointment, "Jeff Bridges")).toBeInTheDocument();
+    expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
+
+    // 8. Check that the DayListItem with the text "Monday" still has the text "1 spots remaining".
     const day = getAllByTestId(container, "day").find((day) =>
       queryByText(day, "Monday")
     );
 
-    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
 });
