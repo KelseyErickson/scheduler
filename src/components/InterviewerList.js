9273e24ddfa87300381c315dad565cd9ab0bf011
KelseyErickson <kerickson@gmx.com>
2021-12-06 18:24:20 -0700

    Remove white space

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "components/InterviewerList.scss"
+import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
 
 function InterviewerList(props) {
-
   const interviewersArray = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
@@ -13,23 +11,21 @@ function InterviewerList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
-        setInterviewer={() => props.onChange(interviewer.id)}  
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
-  })
-  
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">
-        {interviewersArray}
-      </ul>
+      <ul className="interviewers__list">{interviewersArray}</ul>
     </section>
   );
 }
 
 InterviewerList.propTypes = {
-  interviewers: PropTypes.array.isRequired
+  interviewers: PropTypes.array.isRequired,
 };
 
 export default InterviewerList;
