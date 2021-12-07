64a02682b33fab5bdcdddcaac6baccb757b3148c
KelseyErickson <kerickson@gmx.com>
2021-12-06 18:25:59 -0700

    Fix linting errors

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,21 +3,16 @@ import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const { name, avatar, setInterviewer, selected} = props;
-  
-  const interviewerClass = classNames("interviewers__item", 
-  {
-    "interviewers__item--selected": selected, 
+  const { name, avatar, setInterviewer, selected } = props;
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected,
   });
 
-  return(
-    <li className={interviewerClass} onClick={setInterviewer} selected={selected }>
-      <img
-        className="interviewers__item-image"
-        src= { avatar }
-        alt= { name }
-        />
+  return (
+    <li className={interviewerClass} onClick={setInterviewer} selected={selected}>
+      <img className="interviewers__item-image" src={avatar} alt={name} />
       {selected && name}
     </li>
-    );
+  );
 };
