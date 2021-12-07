0583e40698fc52c649f85303295d09c74d8efa5a
KelseyErickson <kerickson@gmx.com>
2021-12-06 18:29:09 -0700

    Add comments back and fix linting errors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,48 +1,46 @@
+// Function to take in state and day and return the appointments for that day as an array of objects
 export function getAppointmentsForDay(state, day) {
   let appointments = [];
-  state.days.forEach(value =>{
-    if(value.name === day) {
+  state.days.forEach((value) => {
+    if (value.name === day) {
       const today = value;
-      today.appointments.forEach(appt => {
-        appointments.push(state.appointments[appt])
-      })
+      today.appointments.forEach((appt) => {
+        appointments.push(state.appointments[appt]);
+      });
     }
-  })
-    
+  });
   return appointments;
-};
+}
 
-
+// Function to take in state and day return the interviewers for that day as an array of objects
 export function getInterviewersForDay(state, day) {
-  let interviewers= [];
+  let interviewers = [];
 
-  state.days.forEach(value =>{
-    if(value.name === day) {
+  state.days.forEach((value) => {
+    if (value.name === day) {
       const today = value;
-      today.interviewers.forEach(int => {
-        interviewers.push(state.interviewers[int])
-      })
+      today.interviewers.forEach((int) => {
+        interviewers.push(state.interviewers[int]);
+      });
     }
-  })
-    
+  });
   return interviewers;
-};
+}
 
 // Function to take in state and interview and return an object with interviewer and student
 export function getInterview(state, interview) {
-  if(!interview){
+  if (!interview) {
     return null;
   }
   const interviewerInfo = state.interviewers[interview.interviewer];
 
-  const interviewObject ={
-    "student": interview.student,
-    "interviewer": {
-      "id": interview.interviewer, 
-      "name": interviewerInfo.name, 
-      "avatar": interviewerInfo.avatar
-    }
+  const interviewObject = {
+    student: interview.student,
+    interviewer: {
+      id: interview.interviewer,
+      name: interviewerInfo.name,
+      avatar: interviewerInfo.avatar,
+    },
   };
-  
   return interviewObject;
-};
+}
